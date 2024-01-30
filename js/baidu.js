92a4cc4b5acca9880522be3981764bd10bb8e753
YCL <121530406+YCL0609@users.noreply.github.com>
2024-01-30 17:01:58 +0800

    Delete js directory

--- js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?9ee9ca6e564351b723d63db903b916f6";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
